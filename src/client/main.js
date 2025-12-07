191a57705351057e685c87e4c28f412d3e8ae5d5
ioseliev <ioseliev@pm.me>
2025-12-07 00:18:42 +0000

    Add initial conditional CSR
    Signed-off-by: ioseliev <ioseliev@pm.me>

--- src/client/main.js
+++ src/client/main.js
@@ -1,8 +1,33 @@
 import "./style.css";
 
 
-const needs_login = true;
+const app = document.querySelector("#app");
+
+const queryString = new URLSearchParams(window.location.search);
+const needs_login = !queryString.has('code');
 
 (() => {
-  document.querySelector('#app').innerHTML = `<p>${import.meta.env.VITE_REDIRECT_URI}</p>`;
+  const login_btn  = document.createElement("a");
+  const logout_btn = document.createElement("a");
+  const repos_ul   = document.createElement("ul");
+
+  login_btn.className = logout_btn.className = 'btn';
+
+  // Login button
+  login_btn.innerText = "Log in with GitHub";
+  login_btn.href = "/?code=..."; // TODO
+
+  // Logout button
+  logout_btn.innerText = "Log out";
+  logout_btn.href = "/";
+
+  // Repos div
+  repos_ul.id = "repos";
+
+  if (needs_login) {
+    app.appendChild(login_btn);
+  } else {
+    app.appendChild(logout_btn);
+  }
+  app.appendChild(repos_ul);
 })();
